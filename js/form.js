6685e71f498bfdd09ed4aed7ba603de647f9ed09
Ayman-0-Ahmed <aymanahmedpersonal@gmail.com>
2021-08-14 23:17:31 +0600

    Stage

--- js/form.js
+++ js/form.js
@@ -11,13 +11,14 @@ function validate() {
     } else {
         if (/^[a-zA-Z][a-zA-Z0-9_.]{5,29}$/.test(email_username)) {
           alert("This is a valid Username.");
+        } else if (email_username.length == 0) {
         } else if (email_username.length < 6) {
           alert("This username has to be bigger than 6 charecters.");
         } else if (email_username.length > 30) {
           alert("This username has to be smaller than 30 charecters.");
         } else if (/^[0-9_.]$/.test(email_username.charAt(0))) {
           alert("The first should not be a number or a underscore or period")
-        }else {
+        } else {
           alert("illegal characters.");
         }
     }
