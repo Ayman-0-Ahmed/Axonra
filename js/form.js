21fdb0b44e854650364988930db45f892f471763
Ayman-0-Ahmed <aymanahmedpersonal@gmail.com>
2021-09-20 21:57:45 +0600

    Commit

--- js/form.js
+++ js/form.js
@@ -1,30 +1,30 @@
-var email_username = document.getElementById('fname-femail').value;
-let form_password = document.getElementById('fpassword');
+var email_username = document.getElementById('name-email').value;
+let form_password = document.getElementById('password');
 var show_icon = document.getElementById('show_icon');
 var hidden = true;
 
 function validate() {
-    if (/\@/.test(email_username)) {
-        if (/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(email_username)) {
-            return (true)
-        } else {
-          alert("You have entered an invalid email address!");
-          return (false)
-        }
+  if (/\@/.test(email_username)) {
+    if (/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(email_username)) {
+      return (true)
     } else {
-        if (/^[a-zA-Z][a-zA-Z0-9_.]{5,29}$/.test(email_username)) {
-          alert("This is a valid Username.");
-        } else if (email_username.length == 0) {
-        } else if (email_username.length < 6) {
-          alert("This username has to be bigger than 6 charecters.");
-        } else if (email_username.length > 30) {
-          alert("This username has to be smaller than 30 charecters.");
-        } else if (/^[0-9_.]$/.test(email_username.charAt(0))) {
-          alert("The first should not be a number or a underscore or period")
-        } else {
-          alert("illegal characters.");
-        }
+      alert("You have entered an invalid email address!");
+      return (false)
     }
+  } else {
+    if (/^[a-zA-Z][a-zA-Z0-9_.]{5,29}$/.test(email_username)) {
+      alert("This is a valid Username.");
+    } else if (email_username.length == 0) {
+    } else if (email_username.length < 6) {
+      alert("This username has to be bigger than 6 charecters.");
+    } else if (email_username.length > 30) {
+      alert("This username has to be smaller than 30 charecters.");
+    } else if (/^[0-9_.]$/.test(email_username.charAt(0))) {
+      alert("The first should not be a number or a underscore or period")
+    } else {
+      alert("illegal characters.");
+    }
+  }
 }
 
 function show_hide() {
--- js/shop.js
+++ js/shop.js
@@ -3,9 +3,9 @@ function options() {
 }
 
 function load() {
-  let elem = document.getElementById("products");
+  let elem = document.getElementById('main');
 
-  for (var i = 0; i < 3; i++) {
+  for (var i = 0; i < 15; i++) {
     var random_link = Math.floor(Math.random() * 3) + 1;
     elem.innerHTML += '<div class="product_display"><img class="product_image" src="products/' + random_link + '.svg"><div class="product_name">Car</div><div class="product_descreption">This is a 3D Model of a Car</div></div>';
   }
